c28ec681d19eb205ec67d5e1e33f70aaae2cccf2
Abe <ai_980@inbox.ru>
2023-02-11 12:58:43 +0300

    закончено дз

--- Program.cs
+++ Program.cs
@@ -45,3 +45,42 @@
 
 
 // Console.WriteLine("max = " + max);
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number%2 == 0)
+// {
+//     Console.WriteLine("Число " + number + " четное");
+// }
+// else{
+//     Console.WriteLine("Число " + number + " нечетное");
+// }
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+if (N < 2) // если N включаем в диапазон, если нет, то <=
+{
+    Console.WriteLine("Нет четных четных чисел в диапазоне");
+}
+else
+{
+    int start = 2;
+    while (start <= N) // если N не включаем, то <
+    {
+        Console.WriteLine(start);
+        start += 2;
+    }
+}
