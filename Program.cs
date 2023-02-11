001082eb1953a050ab2e7c8468138a2f057e56b0
Abe <ai_980@inbox.ru>
2023-02-11 12:03:16 +0300

    две задачи

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber > secondNumber)
+// {
+//     Console.WriteLine("Первовое число " + firstNumber + 
+//                     " больше, а второе число " + secondNumber + " меньше");
+// }
+// else if (firstNumber < secondNumber)
+// {
+//     Console.WriteLine("Второе число " + secondNumber + 
+//                     " больше, а первое число " + firstNumber + " меньше");
+// }
+// else
+// {
+//     Console.WriteLine("Первовое число " + firstNumber + 
+//                     " равно, второму числу " + secondNumber);
+// }
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа
+//  и выдаёт максимальное из этих чисел.
+
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите третее число: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = number1;
+
+
+// if (number2 > max) max = number2;
+// if (number3 > max) max = number3;
+
+
+// Console.WriteLine("max = " + max);
